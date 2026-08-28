5acb412399b5f1f0f3b346a023a4acca6f2b42a0
chail233 <1744315424@qq.com>
2026-08-28 17:39:55 +0800

    feat:add config.example.ts

---
+++ chater/config.example.ts
@@ -0,0 +1,15 @@
+
+const historyLim = 100;
+const baseURL = "https://***/compatible-mode/v1";
+const model = "";
+const temperature = 0.2;
+const appPort = 1145;
+const apikey = "";
+export default {
+    apikey:apikey,
+    historyLim:historyLim,
+    baseURL:baseURL,
+    model:model,
+    temperature:temperature,
+    appPort:appPort,
+}
